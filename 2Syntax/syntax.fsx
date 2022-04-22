e131495547cc389b85119046ad5231492c69fba5
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-04-22 11:42:51 +0200

    Update syntax.fsx

--- 2Syntax/syntax.fsx
+++ 2Syntax/syntax.fsx
@@ -33,4 +33,54 @@ let squareAndDouble x =
     double y // no return keyword
 
 
+(*
+How to convert from a C-style language to F#
+    * change to "let ... =" for definitionns
+    * indent how you normally would
+    * delete the { }
+    * delete "return"
+    * delete semicolons
+    * you often can delete type annotations as well!
+*)
 
+// C-style language
+(*
+int squareAndDouble(int x)
+{
+    var y = square(x);
+    return double(y);
+}
+
+// converted to F#-style
+
+let squareAndDouble x =
+    let y = square x
+    double y
+*)
+
+// instances where { } are used
+// the curly bracers after async is a computation expression
+
+let downloadManyFiles() =
+    async {
+        let! contetsA = downloadFile "source/a.txt"
+        do! uploadFile contentsA "target/a.txt"
+        return "OK"
+    }
+
+    // the ! after let is called "bang"
+    // in an "async" computation expression:
+    // let! is like "await"
+    // do! is like "await void"
+
+    (*
+    computation expressions are used for:
+    * async/task
+    * queries (like query expressions in C#)
+    * generation collections/enums
+    * validation
+    * error handling
+    * testing
+    * code generation
+    * etc etc
+    *)
