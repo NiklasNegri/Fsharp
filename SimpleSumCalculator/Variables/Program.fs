cb5320fbb125d2e872c04aa3df559e0ea6886b25
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-04-27 18:18:09 +0200

    created a simple sum calculator

---
+++ SimpleSumCalculator/Variables/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "F# Calculator initiated" 
+
+    printfn "Type the first number"
+    let firstNo = Console.ReadLine()
+    printfn "Type the second number"
+    let secondNo = Console.ReadLine()
+    printfn "First %s, Second %s" firstNo secondNo
+
+    let sum = (int firstNo) + (int secondNo)
+    printfn "The sum is %i" sum
+    0
