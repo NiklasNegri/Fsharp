c91ba5339486789577c029476eefcdc271d4e505
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-04-27 12:48:02 +0200

    Update types.fsx
    finished first tutorial lessons

--- types.fsx
+++ types.fsx
@@ -18,4 +18,59 @@ let anotherThing = { aThing with Id=2 }
 
 let yetAnotherThing = { anotherThing with Description="yet another thing" }
 
-let desc = yetAnotherThing.Description
+// anonymous record construction -- type definitions not needed
+// useful when sending JSON data for example
+
+let aContact = {| Name="Scott"; Email="scott@example.com"; |}
+
+// ==================
+// discriminated unions aka "choice" types "OR" types
+// ==================
+
+// a PrimaryColor is Red OR Yellow OR Blue
+type PrimaryColor = Red | Yellow | Blue
+
+// you can assign a data type to each choice
+type RGB = {R:int; G:string; B:bool}
+
+type Color =
+    | Primary of PrimaryColor
+    | RGB of RGB
+    | Name of string
+
+// testing around
+let aColor = Red
+let bColor = { R=5; G="s"; B=true }
+let cColor = Name "Purple"
+
+// =================
+// composing with types (like lego)
+// =================
+
+(*
+Some requirements:
+
+We accept three payment methods:
+Cash, PayPal or Card
+
+For Cash we dont need any extra information
+For PayPal we need an email address
+For Cards we need a card type and card number
+
+How to implement this?
+*)
+
+module PaymentDomain =
+
+    type EmailAddress = string
+    type CardType = Visa | Mastercard
+    type CardNumber = string
+
+    type CreditCardInfo = {
+        CardType : CardType
+        CardNumber : CardNumber
+    }
+    type PaymentMethod =
+        | Cash
+        | PayPal of EmailAddress
+        | Card of CreditCardInfo
